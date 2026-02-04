607156c8b1f008a41f8825bb6763139df79e6b5a
Abhishek <152371724+Abhisheklohar0@users.noreply.github.com>
2026-02-05 01:43:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 This contains everything you need to run your app locally.
 
-View your app in AI Studio: https://ai.studio/apps/drive/1tZDtKTgzncuiLvrreYrKVGFQERyGzXos
+View your app in AI Studio: https://ai.studio/apps/drive/1yVD-0LmdZLA1_26bRnRw1pwc019NhveU
 
 ## Run Locally
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  define: {
-    // This allows libraries expecting process.env (like the Gemini SDK) to work in a Vite environment
-    'process.env': {
-      API_KEY: process.env.VITE_API_KEY || ''
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
